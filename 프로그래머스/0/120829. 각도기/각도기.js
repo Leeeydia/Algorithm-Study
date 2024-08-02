e00b6fcc7855c0f79a38a4fccff0ed0bebfcd0d7
YUN <leeeydia@gmail.com>
2024-08-02 13:25:05 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,14 @@
+function solution(angle) {
+   
+    if (angle == 90) {
+    return angle = 2;
+} else if (angle > 90 && angle < 180) {
+    return angle = 3;
+} else if (angle >= 180) {
+    return angle = 4;
+} else {
+    return angle = 1;  
+}
+
+    return answer;
+}
