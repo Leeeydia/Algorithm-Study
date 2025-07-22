b8a380a63b491ddfa04526ff35aa8d18f7376270
YUN <leeeydia@gmail.com>
2025-07-22 22:50:49 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,26 @@
+function solution(array) {
+
+  if (array.length === 1) return array[0];
+
+  const obj = {};
+  const answer = [];
+
+  array.forEach((n) => {
+    obj[n] = ++obj[n] || 1;
+  });
+
+
+  for (let key in obj) {
+    answer.push([key, obj[key]]);
+  }
+
+  answer.sort((a, b) => b[1] - a[1]);
+
+  if (answer.length > 1 && answer[0][1] === answer[1][1]) return -1;
+
+  return Number(answer[0][0]);
+}
+
+console.log(solution([1, 2, 3, 3, 3, 4])); // 3
+console.log(solution([1, 1, 2, 2])); // -1
+console.log(solution([1])); // 1
