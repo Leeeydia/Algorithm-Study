18c193a76bd638fe5f448a26aeda1d78cba53c56
YUN <leeeydia@gmail.com>
2025-11-23 22:51:10 +0900

    [level 0] Title: 분수의 덧셈, Time: 13.68 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,19 +1,6 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = {};
-        
-        int bunja = (numer1 * denom2) + (numer2 * denom1);
-        int bunmo = denom1 * denom2;
-        
-        for(int i = bunja; i>1; i--){
-            if(bunja % i == 0 && bunmo % i == 0){
-                bunja /= i;
-                bunmo /= i;
-            }
-        }
-        
-        answer = new int[]{bunja, bunmo};
-        
         return answer;
     }
 }
